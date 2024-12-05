bcb5d84ef62343719971e0826615e667c79cbb8c
3rfaan <28968837+3rfaan@users.noreply.github.com>
2024-12-05 23:33:26 +0100

    Added full day 4

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,5 +1,46 @@
 advent_of_code::solution!(4);
 
+pub fn part_one(input: &str) -> Option<u32> {
+    let grid = Grid::from(input);
+    let offsets = vec![
+        (0, 1),   // right
+        (0, -1),  // left
+        (1, 0),   // down
+        (-1, 0),  // up
+        (1, 1),   // up left
+        (-1, -1), // up right
+        (1, -1),  // down left
+        (-1, 1),  // down right
+    ];
+
+    Some(
+        (0..grid.rows)
+            .flat_map(|row| (0..grid.cols).map(move |col| (row, col)))
+            .flat_map(|(row, col)| offsets.iter().map(move |dir| (row, col, dir)))
+            .filter(|&(row, col, dir)| grid.xmas_finder(row, col, &dir))
+            .count() as u32,
+    )
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let grid = Grid::from(input);
+
+    Some(
+        (0..grid.rows)
+            .flat_map(|row| (0..grid.cols).map(move |col| (row, col)))
+            .filter(|&(row, col)| grid.cross_finder(row, col))
+            .count() as u32,
+    )
+}
+
+macro_rules! off {
+    ($self:expr,($row:ident $op1:tt $off1:literal),($col:ident $op2:tt $off2:literal)) => {
+        $self.grid
+            .get(($row as isize $op1 $off1) as usize)
+            .and_then(|c| c.get(($col as isize $op2 $off2) as usize))
+    };
+}
+
 struct Grid {
     grid: Vec<Vec<char>>,
     rows: usize,
@@ -7,44 +48,41 @@ struct Grid {
 }
 
 impl Grid {
-    fn get_word_count(&mut self, word: &str) -> Option<u32> {
-        let dirs = Direction::get_dirs();
-        let mut count = 0;
+    fn xmas_finder(&self, row: usize, col: usize, off: &(isize, isize)) -> bool {
+        let word = "XMAS";
+        let (off_x, off_y) = off;
 
-        for row in 0..self.rows {
-            for col in 0..self.cols {
-                for dir in &dirs {
-                    if self.walk(word, row, col, dir) {
-                        count += 1;
-                    }
-                }
-            }
-        }
-        Some(count)
-    }
-
-    fn walk(&self, word: &str, row: usize, col: usize, dir: &Direction) -> bool {
-        let (off_x, off_y) = dir.offset();
-
-        for (word_idx, word_char) in word.chars().enumerate() {
-            let new_row = row as isize + word_idx as isize * off_x;
-            let new_col = col as isize + word_idx as isize * off_y;
-
-            if new_row < 0
-                || new_col < 0
-                || new_row >= self.rows as isize
-                || new_col >= self.cols as isize
-            {
-                return false;
-            }
+        word.chars().enumerate().all(|(i, word_char)| {
+            let new_row = row as isize + i as isize * off_x;
+            let new_col = col as isize + i as isize * off_y;
 
             let (nx, ny) = (new_row as usize, new_col as usize);
 
-            if self.grid[nx][ny] != word_char {
-                return false;
+            match self.grid.get(nx).and_then(|row| row.get(ny)) {
+                Some(&letter) if letter == word_char => true,
+                _ => false,
             }
+        })
+    }
+
+    fn cross_finder(&self, row: usize, col: usize) -> bool {
+        if self.grid[row][col] != 'A' {
+            return false;
         }
-        true
+
+        let ul = off!(self, (row - 1), (col - 1)); // up left
+        let ur = off!(self, (row + 1), (col - 1)); // up right
+        let dl = off!(self, (row - 1), (col + 1)); // down left
+        let dr = off!(self, (row + 1), (col + 1)); // down right
+
+        let (mut ul_to_dr, mut ur_to_dl) = (false, false);
+
+        if let (Some(&ul), Some(&ur), Some(&dl), Some(&dr)) = (ul, ur, dl, dr) {
+            ul_to_dr = (ul == 'M' && dr == 'S') || (ul == 'S' && dr == 'M');
+            ur_to_dl = (ur == 'M' && dl == 'S') || (ur == 'S' && dl == 'M');
+        }
+
+        ul_to_dr && ur_to_dl
     }
 }
 
@@ -55,55 +93,6 @@ impl From<&str> for Grid {
 
         Self { grid, rows, cols }
     }
-}
-
-enum Direction {
-    Right,
-    Left,
-    Down,
-    Up,
-    DiagDownRight,
-    DiagUpLeft,
-    DiagDownLeft,
-    DiagUpRight,
-}
-
-impl Direction {
-    fn offset(&self) -> (isize, isize) {
-        match self {
-            Direction::Right => (0, 1),
-            Direction::Left => (0, -1),
-            Direction::Down => (1, 0),
-            Direction::Up => (-1, 0),
-            Direction::DiagDownRight => (1, 1),
-            Direction::DiagUpLeft => (-1, -1),
-            Direction::DiagDownLeft => (1, -1),
-            Direction::DiagUpRight => (-1, 1),
-        }
-    }
-
-    fn get_dirs() -> Vec<Self> {
-        vec![
-            Direction::Right,
-            Direction::Left,
-            Direction::Down,
-            Direction::Up,
-            Direction::DiagDownRight,
-            Direction::DiagUpLeft,
-            Direction::DiagDownLeft,
-            Direction::DiagUpRight,
-        ]
-    }
-}
-
-pub fn part_one(input: &str) -> Option<u32> {
-    let word = "XMAS";
-
-    Grid::from(input).get_word_count(word)
-}
-
-pub fn part_two(input: &str) -> Option<u32> {
-    None
 }
 
 #[cfg(test)]
